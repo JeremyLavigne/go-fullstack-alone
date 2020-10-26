a0ed4c1a08410a5aee79258a63e565d45d89b0f9
JeremyLavigne <lavigne.jeremy@post.com>
2020-10-26 14:50:52 +0100

    Set up front end with react, webpack and babel.

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+
+    return (
+        <div>
+            <button>
+                Click me to try if server answer.
+            </button>
+        </div>
+    )
+};
+
+export default App;
